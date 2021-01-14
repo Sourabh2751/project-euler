6e5f2300ce1c7dbd8f4ff4f7624802c49dc906db
Sourabh2751 <77433486+Sourabh2751@users.noreply.github.com>
2021-01-14 23:42:19 +0530

    Update PROBLEM1.java

--- PROBLEM1.java
+++ PROBLEM1.java
@@ -1,6 +1,7 @@
-package projecteuler;
+
 /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.*/
+
+Find the sum of all the multiples of 3 or 5 below 1000*/
  public class Question1
 {
 	public static void main (String args[])
@@ -17,3 +18,4 @@ Find the sum of all the multiples of 3 or 5 below 1000.*/
 
 }
 }
+ //output Sum of multiple of 3 and 5 are=233168
