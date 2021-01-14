e1955b042c3d9e755890d56b48d0c8d7a3b618b5
Sourabh2751 <77433486+Sourabh2751@users.noreply.github.com>
2021-01-14 23:43:20 +0530

    Create PROBLEM2.java

---
+++ PROBLEM2.java
@@ -0,0 +1,23 @@
+
+/*By considering the terms in the Fibonacci sequence 
+ * whose values do not exceed four million, find the sum of the even-valued terms.*/
+
+public class p2
+{
+	public static void main (String args[])
+{
+	int a=0,b=1,fib=0,sum=0,c=4000000;
+	while(fib<=c)
+	{
+		fib=a+b;
+		a=b;
+		b=fib;
+		if(fib%2==0)
+		{
+			sum=sum+fib;
+		}
+	}
+	System.out.println("Sum of even Fibonacci numbers is="+sum);
+}
+}
+//output Sum of even Fibonacci numbers is=4613732
