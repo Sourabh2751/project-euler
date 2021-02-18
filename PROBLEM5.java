6310330137193ac74a2405029f878789069e3ca1
Sourabh2751 <77433486+Sourabh2751@users.noreply.github.com>
2021-02-18 22:26:05 +0530

    Update PROBLEM5.java

--- PROBLEM5.java
+++ PROBLEM5.java
@@ -1,7 +1,7 @@
 package projecteuler;
 import java.math.BigInteger;
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-public class p5 {
+public class PROBLEM5 {
 	public static void main(String[] args) {
 		p5 ob = new p5();
 	System.out.println(ob.multiple());
