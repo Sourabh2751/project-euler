4abb9c3f04dfc156abd60ff9919f2b8a016491b8
Sourabh2751 <77433486+Sourabh2751@users.noreply.github.com>
2021-02-18 22:26:38 +0530

    Update PROBLEM6.java

--- PROBLEM6.java
+++ PROBLEM6.java
@@ -1,6 +1,6 @@
 
 //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-public class p6 {
+public class PROBLEM6 {
 	
 		public static void main(String args[])
 		{
